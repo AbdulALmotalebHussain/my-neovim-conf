766f7372ef9e9f5ece7facb4bcb833693b58d374
AbdulALmotalebHussain <155813788+AbdulALmotalebHussain@users.noreply.github.com>
2024-07-07 23:49:30 +0300

    Update colors.lua

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,9 @@
 require('rose-pine').setup({
-    disable_background = true
+    disable_background = false
 })
 
-function ColorMyPencils(color) 
-	color = color or "rose-pine"
+function ColorMyPencils(color)
+	color = color or "gruvbox-minor"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -12,3 +12,4 @@ function ColorMyPencils(color)
 end
 
 ColorMyPencils()
+
